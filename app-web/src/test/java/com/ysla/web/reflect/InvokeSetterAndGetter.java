1367ddab4d098859e48af9232efe069a04e9249e
konghanghang <ysla@pm.me>
2018-10-22 17:05:31 +0800

    反射的一些用法

---
+++ app-web/src/test/java/com/ysla/web/reflect/InvokeSetterAndGetter.java
@@ -0,0 +1,31 @@
+package com.ysla.web.reflect;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InvokeSetterAndGetter {
+
+    @Test
+    public void test() throws Exception{
+        //Class clazz = Class.forName("com.ysla.web.reflect.User");
+        //Object obj = clazz.newInstance();
+        User user = new User();
+        user.setUsername("konghang");
+
+        PropertyDescriptor pd = new PropertyDescriptor("username", User.class);
+        Method get = pd.getReadMethod();
+        String name = (String)get.invoke(user);
+        Assert.assertEquals("konghang",name);
+        Method write = pd.getWriteMethod();
+        write.invoke(user,"konghang1");
+        name = get.invoke(user).toString();
+        Assert.assertEquals("konghang1",name);
+    }
+
+}
